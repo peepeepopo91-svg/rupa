ad6296f1455ce2bee01d30c374877e5f4e44b528
bluegaming08 <qasimdevmc@gmail.com>
2026-07-08 13:23:52 +0500

    Update players.ts

--- src/data/players.ts
+++ src/data/players.ts
@@ -65,9 +65,9 @@ const players: Player[] = [
       uhc: "LT5",
       crystal: "HT5",
       mace: "LT3",
-      axe: "null",
-      nethpot: "null",
-      diapot: "null",
+      axe: "NONE",
+      nethpot: "NONE",
+      diapot: "NONE",
     },
   },
 ]
